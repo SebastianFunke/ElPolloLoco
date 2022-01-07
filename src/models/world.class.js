900a74278f9b0dbfd9a80ec036a26be6c1bae397
unknown <funke.sebastian@web.de>
2022-01-07 07:04:49 +0100

    added comments

--- src/models/world.class.js
+++ src/models/world.class.js
@@ -16,6 +16,11 @@ class World {
     bottleThrowed = false;
     pickableBottles = [];
 
+    /**
+     * function is called when object is generated
+     * this function sets the main abilities
+     * @param {ctx} canvas 
+     */
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -34,51 +39,92 @@ class World {
         this.setBottles();
         this.main();
         this.checkBgMove();
-
     }
 
+    /**
+     * sets the width of the infoText-div
+     */
     setInfoText() {
         document.getElementById('infoText').style.width = properties.width + 'px';
     }
 
+    /**
+     * generate a new character object
+     */
     setCharacter() {
         this.character = new Character(this.canvas.width, this.maxSpeed);
     }
 
+    /**
+     * generate new background objects, as many in level 1 class are defined
+     */
     setBackgroundObjects() {
-        this.backgroundObjects = level1.backgroundObjects;
-    }
-
+            this.backgroundObjects = level1.backgroundObjects;
+        }
+        /**
+         * generate new cloud objects, as many in level 1 class are defined
+         */
     setClouds() {
         this.cloudObjects = level1.clouds;
     }
 
+    /**
+     * generate new enemy objects, as many in level 1 class are defined
+     */
     setEnemies() {
         this.enemies = level1.enemies;
     }
 
+    /**
+     * generate new coin objects, as many in level 1 class are defined
+     */
     setCoins() {
         this.coins = level1.coins;
     }
 
+    /**
+     * generate a new boss object
+     */
     setEndBoss() {
         this.endBoss = new EndBoss();
     }
 
+    /**
+     * generate new statusbar objects, three for character and one for boss
+     */
     setStatusBar() {
         this.statusBar.push(new StatusBar('life'));
         this.statusBar.push(new StatusBar('bottle'));
         this.statusBar.push(new StatusBar('coin'));
         this.statusBar.push(new StatusBar('boss'));
-
     }
 
+    /**
+     * generate new pickable bottle objects, as many in level 1 class are defined
+     */
     setBottles() {
         this.pickableBottles = level1.bottles;
     }
 
+    /**
+     * main function
+     * clear the canvas and call several functions
+     */
     main() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawObjects();
+        this.checkColissions();
+        this.checkGenerateBottle();
+        let self = this;
+        requestAnimationFrame(function() {
+            self.main();
+        });
+    }
+
+    /**
+     * main function to draw objects by doing calling specialized functions
+     */
+    drawObjects() {
         this.drawBackgroundObjects();
         this.drawClouds();
         this.drawEnemies();
@@ -88,20 +134,20 @@ class World {
         this.drawStatusBars();
         this.drawBottle();
         this.drawpickableBottles();
-        this.checkColissions();
-        this.checkGenerateBottle();
-        let self = this;
-        requestAnimationFrame(function() {
-            self.main();
-        });
     }
 
+    /**
+     * function to draw the backgrounds
+     */
     drawBackgroundObjects() {
         this.backgroundObjects.forEach(backgroundObject => {
             this.ctx.drawImage(backgroundObject.img, backgroundObject.x, 0, this.canvas.width, this.canvas.height);
         });
     }
 
+    /**
+     * function to draw clouds
+     */
     drawClouds() {
         this.cloudObjects.forEach(cloud => {
             cloud.moveCloudLeft(this.canvas.width, this.speedBgLayerFour);
@@ -109,12 +155,18 @@ class World {
         });
     }
 
+    /**
+     * function to draw enemies
+     */
     drawEnemies() {
         this.enemies.forEach(enemy => {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
         });
     }
 
+    /**
+     * function to draw pickable coins
+     */
     drawCoins() {
         this.coins.forEach(coin => {
             this.ctx.drawImage(coin.img, coin.x, coin.y, coin.width, coin.height);
@@ -126,6 +178,9 @@ class World {
         });
     }
 
+    /**
+     * function to draw pickable bottles
+     */
     drawpickableBottles() {
         this.pickableBottles.forEach(pickablebottle => {
             this.ctx.drawImage(pickablebottle.img, pickablebottle.x, pickablebottle.y, pickablebottle.width, pickablebottle.height);
@@ -137,6 +192,10 @@ class World {
         });
     }
 
+    /**
+     * function to draw the character 
+     * rotates the picture depending on which direction you are walking
+     */
     drawCharacter() {
         if (!this.character.lookDirection) {
             this.ctx.save();
@@ -151,10 +210,16 @@ class World {
         }
     }
 
+    /**
+     * function to draw the endboss
+     */
     drawEndBoss() {
         this.ctx.drawImage(this.endBoss.img, this.endBoss.x, this.endBoss.y, this.endBoss.width, this.endBoss.height);
     }
 
+    /**
+     * function to draw the status bars
+     */
     drawStatusBars() {
         this.statusBar.forEach(element => {
             element.setImg(this.character.energy, this.character.bottles, this.character.coins, this.endBoss.energy);
@@ -162,6 +227,10 @@ class World {
         });
     }
 
+    /**
+     * function to draw the bottles
+     * if a bottle is picked, its energy is zero. then the bottle will be deleted
+     */
     drawBottle() {
         this.bottles.forEach(function(item, index, object) {
             if (item.energy <= 0) {
@@ -171,22 +240,50 @@ class World {
         this.bottles.forEach(element => {
             this.ctx.drawImage(element.img, element.x, element.y, element.width, element.height);
         });
-
     }
 
-
+    /**
+     * function to get the canvas height
+     * @returns number
+     */
     getHeight() {
         return this.canvas.height;
     }
 
+    /**
+     * function to get the canvas width
+     * @returns number
+     */
     getWidth() {
         return this.canvas.width;
     }
 
+    /**
+     * returns the max speed of the game
+     * @returns number
+     */
     getMaxSpeed() {
         return this.maxSpeed;
     }
 
+    /**
+     * main function for collisions. 
+     * calls single specific functions 
+     */
+    checkColissions() {
+        this.checkEnemiesSmashed();
+        this.checkEnemiesHigher();
+        this.checkEnemiesColiding();
+        this.checkBottles();
+        this.checkCoins();
+        this.checkPickableObjects();
+    }
+
+    /**
+     * checks if the enemy is smashed by player
+     * in this case the enemies energy is set to zero
+     * and dead images will be set
+     */
     checkEnemiesSmashed() {
         this.enemies.forEach((enemy) => {
             if (this.character.isSmashed(enemy) && enemy.energy > 0) {
@@ -197,6 +294,10 @@ class World {
         });
     }
 
+    /**
+     * if the character if jumping and above an enemie,
+     * the variable isHigher will set to true
+     */
     checkEnemiesHigher() {
         this.enemies.forEach((enemy) => {
             if (this.character.checkHigher(enemy)) {
@@ -207,6 +308,9 @@ class World {
         });
     }
 
+    /**
+     * checks if the character is collided with an enemy
+     */
     checkEnemiesColiding() {
         this.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy) && enemy.energy > 0) {
@@ -215,6 +319,9 @@ class World {
         });
     }
 
+    /**
+     * checks if a throwed bottle hited the boss
+     */
     checkBottles() {
         this.bottles.forEach((bottle) => {
             if (this.endBoss.isColliding(bottle)) {
@@ -226,6 +333,10 @@ class World {
         });
     }
 
+    /**
+     * checks if the character collides with a coin
+     * if true, sets energy of coin to zero and adds a coin to character
+     */
     checkCoins() {
         this.coins.forEach((coin) => {
             if (this.character.isColliding(coin)) {
@@ -236,6 +347,10 @@ class World {
         });
     }
 
+    /**
+     * checks if the character collides with a pickable bottle
+     * if true, sets energy of pickable bottle to zero and adds a pickable bottle to character
+     */
     checkPickableObjects() {
         this.pickableBottles.forEach((pickableBottle) => {
             if (this.character.isColliding(pickableBottle)) {
@@ -246,76 +361,85 @@ class World {
         });
     }
 
-    checkColissions() {
-        this.checkEnemiesSmashed();
-        this.checkEnemiesHigher();
-        this.checkEnemiesColiding();
-        this.checkBottles();
-        this.checkCoins();
-        this.checkPickableObjects();
-    }
-
-
-
+    /**
+     * function to move the background if character is walking and alive
+     */
     checkBgMove() {
         setInterval(() => {
-
-
-            this.enemies.forEach(enemy => {
-                enemy.setAdditionalSpeed(0);
-            });
-            this.bottles.forEach(bottle => {
-                bottle.setAdditionalSpeed(0);
-            });
-
-
+            setAdditionalSpeedZero();
             if (!this.character.isDead()) {
                 if (!this.character.checkBothDirectionKeysPressed()) {
                     if (this.character.rightEnd && keyboard.getPressedKey('right')) {
-                        this.endBoss.moveLeft();
-                        this.bottles.forEach(bottle => {
-                            bottle.setAdditionalSpeed(this.maxSpeed * -1);
-                        });
-
-                        this.enemies.forEach(enemy => {
-                            enemy.setAdditionalSpeed(this.maxSpeed);
-                        });
-                        this.backgroundObjects.forEach(object => {
-                            object.moveBgRight(this.canvas.width, this.maxSpeed);
-                        });
-
-                        this.coins.forEach(coin => {
-                            coin.moveRight(this.maxSpeed);
-                        });
-                        this.pickableBottles.forEach(pickableBottle => {
-                            pickableBottle.moveRight(this.maxSpeed);
-                        });
+                        this.moveCanvasRight();
                     }
                     if (this.character.leftEnd && keyboard.getPressedKey('left')) {
-                        this.endBoss.moveRight();
-                        this.enemies.forEach(enemy => {
-                            enemy.setAdditionalSpeed(this.maxSpeed * -1);
-                        });
-                        this.backgroundObjects.forEach(object => {
-                            object.moveBgLeft(this.canvas.width, this.maxSpeed);
-                        });
-                        this.bottles.forEach(bottle => {
-                            bottle.setAdditionalSpeed(this.maxSpeed);
-                        });
-                        this.coins.forEach(coin => {
-                            coin.moveLeft(this.maxSpeed);
-                        });
-                        this.pickableBottles.forEach(pickableBottle => {
-                            pickableBottle.moveLeft(this.maxSpeed);
-                        });
+                        this.moveCanvasLeft();
                     }
-
                 }
             }
         }, 1000 / 60);
-
     }
 
+    /**
+     * sets the additional speed of bottles and enemies to zero
+     */
+    setAdditionalSpeedZero() {
+        this.enemies.forEach(enemy => {
+            enemy.setAdditionalSpeed(0);
+        });
+        this.bottles.forEach(bottle => {
+            bottle.setAdditionalSpeed(0);
+        });
+    }
+
+    /**
+     * when character is walking to right, lets move the background to left
+     */
+    moveCanvasLeft() {
+        this.endBoss.moveRight();
+        this.enemies.forEach(enemy => {
+            enemy.setAdditionalSpeed(this.maxSpeed * -1);
+        });
+        this.backgroundObjects.forEach(object => {
+            object.moveBgLeft(this.canvas.width, this.maxSpeed);
+        });
+        this.bottles.forEach(bottle => {
+            bottle.setAdditionalSpeed(this.maxSpeed);
+        });
+        this.coins.forEach(coin => {
+            coin.moveLeft(this.maxSpeed);
+        });
+        this.pickableBottles.forEach(pickableBottle => {
+            pickableBottle.moveLeft(this.maxSpeed);
+        });
+    }
+
+    /**
+     * when character is walking to left, lets move the background to right
+     */
+    moveCanvasRight() {
+        this.endBoss.moveLeft();
+        this.bottles.forEach(bottle => {
+            bottle.setAdditionalSpeed(this.maxSpeed * -1);
+        });
+        this.enemies.forEach(enemy => {
+            enemy.setAdditionalSpeed(this.maxSpeed);
+        });
+        this.backgroundObjects.forEach(object => {
+            object.moveBgRight(this.canvas.width, this.maxSpeed);
+        });
+        this.coins.forEach(coin => {
+            coin.moveRight(this.maxSpeed);
+        });
+        this.pickableBottles.forEach(pickableBottle => {
+            pickableBottle.moveRight(this.maxSpeed);
+        });
+    }
+
+    /**
+     * on pressing button d the character will throw a bottle, if he has enough bottles
+     * this function generates a bottle on position of the character
+     */
     checkGenerateBottle() {
         setInterval(() => {
             if (this.character.bottles > 0) {
@@ -330,6 +454,5 @@ class World {
             }
         }, 1000 / 60);
     }
-
 
 }
